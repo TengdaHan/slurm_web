164999a9df87585e618e1438e7e045648f99a82b
Tengda Han <htd95@126.com>
2022-02-28 11:09:54 +0000

    show gmem

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import os
 from datetime import datetime
 import argparse
 import copy
+from collections import defaultdict
 from subprocess import STDOUT, check_output
 from flask import Flask, Response, render_template_string
 from slurm_gpustat import resource_by_type, parse_all_gpus, gpu_usage, \
-    node_states, INACCESSIBLE, parse_cmd, avail_stats_for_node
+    node_states, INACCESSIBLE, parse_cmd, avail_stats_for_node, parse_node_names
 
 
 # from https://developer.nvidia.com/cuda-gpus
@@ -14,7 +15,10 @@ CAPABILITY = {'a100': 8.0, 'a40': 8.6, 'a30': 8.0, 'a10': 8.6, 'a16': 8.6,
               'v100': 7.0, 'gv100gl': 7.0,
               'p40': 6.1, 'm40': 5.2,
               'rtx6k': 7.5, 'rtx8k': 7.5}
-
+GMEM = {'a40': '[48g]', 'a30': '[24g]',
+        'v100': '[16g]', 'gv100gl': '[32g]',
+        'p40': '[24g]', 'm40': '[12/24g]',
+        'rtx6k': '[24g]', 'rtx8k': '[48g]'}
 
 def get_resource_bar(avail, total, text='', long=False):
     """Create a long/short progress bar with text overlaid. Formatting handled in css."""
@@ -57,6 +61,141 @@ def parse_leaderboard():
         summary_str = ''.join([f'{i:12s}' for i in user_summary])
         out += f"{user:12s}[{total}]    {summary_str}\n"
     return out
+
+
+def cpu_usage(resources, partition='compute'):
+    """Build a data structure of the CPU resource usage, organised by user.
+
+    Args:
+        resources (dict :: None): a summary of cluster resources, organised by node name.
+
+    Returns:
+        (dict): a summary of resources organised by user (and also by node name).
+    """
+    cmd = "squeue -O NumNodes:100,nodelist:100,username:100,jobid:100 --noheader"
+    if partition:
+        cmd += f" --partition={partition}"
+    rows = parse_cmd(cmd)
+    usage = defaultdict(dict)
+    for row in rows:
+        tokens = row.split()
+        # ignore pending jobs
+        if len(tokens) < 4:
+            continue
+        cpu_count_str, node_str, user, jobid = tokens
+        num_cpus = int(cpu_count_str.strip())
+        node_names = parse_node_names(node_str)
+        for node_name in node_names:
+            # If a node still has jobs running but is draining, it will not be present
+            # in the "available" resources, so we ignore it
+            if node_name not in resources:
+                continue
+            cpu_type = resources[node_name]["type"]
+
+            if cpu_type in usage[user]:
+                usage[user][cpu_type][node_name]['n_cpu'] += num_cpus
+            else:
+                usage[user][cpu_type] = defaultdict(lambda: {'n_cpu': 0,})
+                usage[user][cpu_type][node_name]['n_cpu'] += num_cpus
+    return usage
+
+
+def parse_cpu_usage_to_table(partition='compute', show_bar=True):
+    """Request sinfo for cnode, parse the output to a html table."""
+
+    node_str = parse_cmd(f"sinfo -o '%1000N' --noheader --partition={partition}")
+    assert isinstance(node_str, list) and len(node_str) == 1
+    node_names = parse_node_names(node_str[0].strip())
+    resources = {k:{'type': k[0:6]+'xx', 'count': 1} for k in node_names}
+    states = node_states(partition=partition)
+    res = {key: val for key, val in resources.items()
+           if states.get(key, "down") not in INACCESSIBLE}
+    res_total = copy.deepcopy(res)
+    usage = cpu_usage(resources=res, partition=partition)
+
+    for subdict in usage.values():
+        for cpu_type, node_dicts in subdict.items():
+            for node_name, user_cpu_count in node_dicts.items():
+                count = res[node_name]["count"]
+                count = max(count - user_cpu_count['n_cpu'], 0)
+                res[node_name]["count"] = count
+
+    res_total_by_type = defaultdict(list)
+    for node, spec in res_total.items():
+        res_total_by_type[spec["type"]].append({"node": node, "count": spec["count"]})
+    
+    res_usage_by_type = defaultdict(list)
+    for node, spec in res.items():
+        res_usage_by_type[spec["type"]].append({"node": node, "count": spec["count"]})
+
+    table_html = []
+    total_cpu_count = 0
+    avail_cpu_count = 0
+
+    # sort cpus from new to old
+    type_list = sorted(list(res_total_by_type.keys()), 
+                       reverse=True)
+
+    # writing the html table
+    for cpu_type in type_list:
+        node_dicts = res_total_by_type[cpu_type]
+        node_names = sorted([i['node'] for i in node_dicts])
+
+        node_summaries = []
+        num_col = []
+
+        for node in node_names:
+            node_name = f'<td>{node}</td>'
+
+            users = [user for user in usage if node in usage[user].get(cpu_type, [])]
+            if len(users):
+                users = f"<td>user: {','.join(users)}</td>"
+            else:
+                users = f"<td>&nbsp</td>"
+            
+            detail_dict = avail_stats_for_node(node)
+            detail_dict = {k: v for k, v in detail_dict.items() if k in ['cpu', 'mem']}
+
+            if show_bar:
+                c_stat = detail_dict['cpu'].split('/')
+                c_stat = [int(i.strip()) for i in c_stat]
+                cpu_bar = get_resource_bar(*c_stat, text=detail_dict['cpu'])
+
+                m_stat = detail_dict['mem'].split('/')
+                m_stat = [str_to_int(i.strip()) for i in m_stat]
+                mem_bar = get_resource_bar(*m_stat, text=detail_dict['mem'], long=True)
+
+                total_cpu_count += c_stat[-1]
+                avail_cpu_count += c_stat[-1] - c_stat[0]
+            else:
+                cpu_bar = detail_dict['cpu']
+                mem_bar = detail_dict['mem']
+            cpu_stat = f"<td>cpu: {cpu_bar}</td>"
+            mem_stat = f"<td>mem: {mem_bar}</td>"
+            
+            node_summary = f'<tr><td>&nbsp</td>{node_name}{cpu_stat}{mem_stat}{users}</tr>'
+            node_summaries.append(node_summary)
+            num_col.append(5)
+            
+
+        type_summary = (f'<tr><td colspan="{max(num_col)}"><b>'
+                        f'{cpu_type}: </b></td></tr>')
+        table_html.append(type_summary)
+        table_html.extend(node_summaries)
+        
+
+    if show_bar:
+        total_bar = get_resource_bar(avail_cpu_count, total_cpu_count, 
+                                    text=f'{avail_cpu_count} / {total_cpu_count}')
+        total_summary = (f'<tr><td colspan="{max(num_col)}"><h3>'
+                     f'Summary: {total_bar} cpus available</h3></td></tr>')
+    else:
+        total_bar = f'{avail_cpu_count}/{total_cpu_count}'
+        total_summary = ''
+    
+    table_html = f"<table>{total_summary}{''.join(table_html)}</table>"
+
+    return table_html
 
 
 def parse_usage_to_table(show_bar=True):
@@ -141,7 +280,7 @@ def parse_usage_to_table(show_bar=True):
         else:
             type_bar = f'{sum(gpu_count_avail.values())}/{sum(gpu_count_total.values())}'
         type_summary = (f'<tr><td colspan="{max(num_col)}"><b>'
-                        f'{gpu_type}: {type_bar} gpus available</b></td></tr>')
+                        f'{gpu_type} {GMEM.get(gpu_type, "")}: {type_bar} gpus available</b></td></tr>')
         table_html.append(type_summary)
         table_html.extend(node_summaries)
         total_gpu_count += sum(gpu_count_total.values())
@@ -241,6 +380,13 @@ def main():
             out = parse_disk_io()
             yield out
         return Response(generate(), mimetype='text')
+
+    # @app.route('/cpu_resource')
+    # def cpu_resource():
+    #     def generate():
+    #         out = parse_cpu_usage_to_table()
+    #         yield out
+    #     return Response(generate(), mimetype='text')
 
     app.run(host=args.host, port=args.port)
 
